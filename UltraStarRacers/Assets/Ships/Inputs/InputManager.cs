a90183c538af58e76b4ab3cf1223b831db20a136
Chaussurre <samuel@corno.com>
2022-09-16 22:34:37 +0200

    fix switch controller mid game

--- UltraStarRacers/Assets/Ships/Inputs/InputManager.cs
+++ UltraStarRacers/Assets/Ships/Inputs/InputManager.cs
@@ -8,20 +8,15 @@ namespace Ship.Controls
 {
     public abstract class InputManager : MonoBehaviour
     {
-        public enum ActionButtonState
-        {
-            NotPressed, Released, Pressed, Held
-        }
-        
         [System.Serializable]
         public struct InputsState
         {
             public double Rotate;
             public bool Accelerate;
             public bool Brake;
-            public ActionButtonState Action1;
-            public ActionButtonState Action2;
-            public ActionButtonState Action3;
+            public bool Action1;
+            public bool Action2;
+            public bool Action3;
         }
 
         public InputsState inputs;
--- UltraStarRacers/Assets/Ships/Inputs/PlayerInputManager.cs
+++ UltraStarRacers/Assets/Ships/Inputs/PlayerInputManager.cs
@@ -6,81 +6,39 @@ using UnityEngine.InputSystem;
 
 namespace Ship.Controls
 {
-    
+
     public class PlayerInputManager : InputManager
     {
-        private PlayerInput playerInput;
+        public PlayerInput playerInput;
 
-        private void Start()
+        public void OnRotate(InputAction.CallbackContext context)
         {
-            playerInput = new PlayerInput();
-            playerInput.asset.Enable();
+            inputs.Rotate = context.action.ReadValue<float>();
         }
 
-        private void Update()
+        public void OnAccelerate(InputAction.CallbackContext context)
         {
-            
-            inputs.Rotate = playerInput.Player.Rotate.ReadValue<Vector2>().x;
-            inputs.Accelerate = playerInput.Player.Accelerate.IsPressed();
-            inputs.Brake = playerInput.Player.Brake.IsPressed();
-            inputs.Action1 = GetInputState(playerInput.Player.Action1);
-            inputs.Action2 = GetInputState(playerInput.Player.Action2);
-            inputs.Action3 = GetInputState(playerInput.Player.Action3);
+            inputs.Accelerate = context.action.IsPressed();
         }
 
-        private ActionButtonState GetInputState(InputAction action)
+        public void OnBrake(InputAction.CallbackContext context)
         {
-            if (action.WasPressedThisFrame())
-                return ActionButtonState.Pressed;
-            if (action.IsPressed())
-                return ActionButtonState.Held;
-            if (action.WasReleasedThisFrame())
-                return ActionButtonState.Released;
-            return ActionButtonState.NotPressed;
+            inputs.Brake = context.action.IsPressed();
         }
 
-        //public void OnRotate(InputAction.CallbackContext context)
-       //{
-       //    inputs.Rotate = context.action.ReadValue<Vector2>().x;
-       //}
-       //
-       //public void OnAccelerate(InputAction.CallbackContext context)
-       //{
-       //    inputs.accelerate = context.action.IsPressed();
-       //}
-       //
-       //public void OnBrake(InputAction.CallbackContext context)
-       //{
-       //    inputs.brake = context.action.IsPressed();
-       //}
-       //public void OnAction1(InputAction.CallbackContext context)
-       //{
-       //    if (context.action.WasPressedThisFrame())
-       //        inputs.Action1 = ActionButtonState.Pressed;
-       //    else if (context.action.IsPressed())
-       //        inputs.Action1 = ActionButtonState.Held;
-       //    else if (context.action.WasReleasedThisFrame())
-       //        inputs.Action1 = ActionButtonState.Released;
-       //}
-       //public void OnAction2(InputAction.CallbackContext context)
-       //{
-       //    if (context.action.WasPressedThisFrame())
-       //        inputs.Action2 = ActionButtonState.Pressed;
-       //    else if (context.action.IsPressed())
-       //        inputs.Action2 = ActionButtonState.Held;
-       //    else if (context.action.WasReleasedThisFrame())
-       //        inputs.Action2 = ActionButtonState.Released;
-       //}
-       //
-       //public void OnAction3(InputAction.CallbackContext context)
-       //{
-       //    if (context.action.WasPressedThisFrame())
-       //        inputs.Action3 = ActionButtonState.Pressed;
-       //    else if (context.action.IsPressed())
-       //        inputs.Action3 = ActionButtonState.Held;
-       //    else if (context.action.WasReleasedThisFrame())
-       //        inputs.Action3 = ActionButtonState.Released;
-       //}
+        public void OnAction1(InputAction.CallbackContext context)
+        {
+            inputs.Action1 = context.action.IsPressed();
+        }
 
+        public void OnAction2(InputAction.CallbackContext context)
+        {
+            inputs.Action2 = context.action.IsPressed();
+        }
+
+        public void OnAction3(InputAction.CallbackContext context)
+        {
+            inputs.Action3 = context.action.IsPressed();
+        }
     }
 }
