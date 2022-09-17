946e2d3a04f593ebe033b7e9466e86301a9e529b
Chaussurre <samuel@corno.com>
2022-09-17 12:27:42 +0200

    Ship Controls

--- UltraStarRacers/Assets/Ships/MouvementControler.cs
+++ UltraStarRacers/Assets/Ships/MouvementControler.cs
@@ -10,6 +10,8 @@ namespace Ship.Controls
         [SerializeField] private InputManager Inputs;
         private Rigidbody RB;
         
+        [SerializeField] private ShipsStats Stats;
+        
         void Start()
         {
             RB = GetComponentInChildren<Rigidbody>();
@@ -17,10 +19,61 @@ namespace Ship.Controls
 
         private void FixedUpdate()
         {
-            if (Inputs.inputs.Accelerate)
+            var inputs = Inputs.inputs;
+            if (inputs.Rotate != 0)
+                Rotate(inputs.Rotate < 0);
+            
+            if (!inputs.Accelerate && !inputs.Brake)
+                NaturalSlow();
+            else
             {
-                RB.AddForce(transform.forward);
+                var forwardSpeed = Vector3.Project(RB.velocity, transform.forward).magnitude;
+                if (inputs.Brake)
+                    Brake();
+                else if (forwardSpeed < Stats.SpeedMax)
+                    NaturalSlow();
+                else
+                    NaturalSideSlow();
+                
+                if (forwardSpeed < Stats.SpeedMax && inputs.Accelerate)
+                    Accelerate();
             }
+        }
+
+        Vector3 SlowSpeed(Vector3 speed, float SlowRate)
+        {
+            return speed * Mathf.Clamp01(1 - SlowRate * Time.fixedDeltaTime);
+        }
+        
+        void NaturalSlow()
+        {
+            RB.velocity = SlowSpeed(RB.velocity, Stats.SlowRate);
+        }
+
+        void NaturalSideSlow()
+        {
+            var forward = Vector3.Project(RB.velocity, transform.forward);
+            var sideSpeed = RB.velocity - forward;
+            sideSpeed = SlowSpeed(sideSpeed, Stats.SlowRate);
+            RB.velocity = sideSpeed + forward;
+        }
+
+        void Accelerate()
+        {
+            RB.velocity += transform.forward * (Stats.Acceleration * Time.fixedDeltaTime);
+        }
+
+        void Brake()
+        {
+            RB.velocity = SlowSpeed(RB.velocity, Stats.BrakeRate);
+        }
+
+        void Rotate(bool rotateLeft)
+        {
+            Quaternion destination = Quaternion.LookRotation(rotateLeft ? -transform.right : transform.right,
+                transform.up);
+            RB.MoveRotation(Quaternion.RotateTowards(RB.rotation, destination, 
+                Stats.RotateSpeed * Time.fixedDeltaTime));
         }
     }
 }
--- UltraStarRacers/Assets/Ships/ShipsStats.cs
+++ UltraStarRacers/Assets/Ships/ShipsStats.cs
@@ -1,21 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Ship.Controls
 {
-    public class ShipsStats : MonoBehaviour
+    [Serializable]
+    public struct ShipsStats
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        [Tooltip("In unit per seconds")]
+        public float SpeedMax;
+        [Tooltip("In unit per seconds²")]
+        public float Acceleration;
+        public float SlowRate;
+        public float BrakeRate;
+        [Tooltip("In degrees per seconds")] 
+        public float RotateSpeed;
     }
 }
